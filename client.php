bb2beaa32348aedb2759da8c807f88b0b12fb44b
Oleg Salionov <salionov@drom.ru>
2013-02-21 17:15:43 +1100

    save work

--- client.php
+++ client.php
@@ -4,7 +4,7 @@ $port = 41234;
 
 while (true) {
     $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-    $msg = "foobaristo";
+    $msg = "foobaristo" . rand(0,1000);
     $len = strlen($msg);
     socket_sendto($socket, $msg, $len, 0, $server, $port);
     socket_close($socket);
--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 var dgram = require("dgram");
 var fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
+var collection = null;
 
- 
-var saveData = function(data) {
-    MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+var connect = MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
         if(!err) { console.log("We are connected"); }   
-        var collection = db.collection('test');
-        var doc1 = {'data' : data};
-        collection.insert(doc1, function(err, result) {
-            
-        });
-    });
-}
+        collection = db.collection('test');
+});
+
+
 
 var stream = fs.createWriteStream("received.json", { flags: 'w',
     encoding: "utf8",
@@ -22,7 +18,10 @@ var server = dgram.createSocket("udp4");
 server.on("message", function (msg, rinfo) {
     console.log("server got: " + msg + " from "  + 
     rinfo.address + ":" + rinfo.port);
-    saveData(msg);
+    console.info(collection);
+    collection.insert({data: msg}, function(err, result) {
+        if (!err) {console.log(err);}
+    });
     //stream.write(msg);
 });
 
