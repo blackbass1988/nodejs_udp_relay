440d8e13d572a67817912008c022aec18fdae57f
Oleg Salionov <salionov@drom.ru>
2013-02-22 13:54:46 +1100

    config minor

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+var memcacheHost = '127.0.0.1:11211';
+var mongoConnect = "mongodb://localhost:27017/test";
+
 var dgram = require("dgram");
 var fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
 var Memcached = require('memcached');
-var memcacheClient = new Memcached('127.0.0.1:11211');
+var memcacheClient = new Memcached(memcacheHost);
 
 var collection = null;
 
-var connect = MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+var connect = MongoClient.connect(mongoConnect, function(err, db) {
         if(!err) { 
             console.log("We are connected"); 
             collection = db.collection('session');
