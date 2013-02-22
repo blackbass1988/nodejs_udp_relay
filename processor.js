50a285702768bde0d8a5677f06dd955ac0c904d4
Oleg Salionov <oleg@salionov.ru>
2013-02-23 00:29:44 +1100

    minor in processor

--- processor.js
+++ processor.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-    process: function (one, two) {
-        one.body = two.body + one.body;
-        return one;
+    process: function (data, delta) {
+        data.body += delta.body;
+        return data;
     }
 }
