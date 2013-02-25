5d8176eb1e79a7e5cf930ab9baf13fb88ba68985
Oleg Salionov <salionov@drom.ru>
2013-02-25 18:08:36 +1100

    example of push

--- client.php
+++ client.php
@@ -1,4 +1,5 @@
 <?php
+//{"ses_id":"863f2adc3f2504cfc4fd655933b1e1c8","ip":"95.154.84.146","client_hash":"863f2adc3f2504cfc4fd655933b1e1c8","reg_time":1361776049,"last_time":1361776049,"pages_count":0,"user_data":{"common":1,"http_referer":1},"countTimes":1,"changeTime":1361776049}
 $server = "127.0.0.1";
 $port = 41234;
 
